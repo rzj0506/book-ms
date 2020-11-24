51da218b9e21fd36d5f17e1299d36d24f6f031aa
Administrator <13172256229@163.com>
2020-11-24 11:11:01 +0800

    2020.11.24 11.10 李宏裕

--- book-ms-HonYu/src/main/java/com/zuiqiang/base_controller/BaseController.java
+++ book-ms-HonYu/src/main/java/com/zuiqiang/base_controller/BaseController.java
@@ -28,7 +28,7 @@ public class BaseController {
 
         User loginUser = (User) request.getSession().getAttribute("loginUser");
 
-       
+       userId = Integer.getInteger(request.getHeader("sessionId"));
 
     }
 }
